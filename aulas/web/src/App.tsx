f096653c8a730a75dca59ea2d06a9ae2ea539b36
dosza <oliveira.daniel109@gmail.com>
2022-05-09 16:51:31 -0300

    inicializa component Widget

--- aulas/web/src/App.tsx
+++ aulas/web/src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
+import { Widget } from "./components/Widget";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <h1>Hello World</h1>
+  <Widget/>
   )
 }
-
-export default App
---
+++ aulas/web/src/components/Widget.tsx
@@ -0,0 +1,4 @@
+export function Widget() {
+    return <h1>Hello Widget!!</h1>
+
+}
