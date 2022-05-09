0c9b730e1d2fd4edd139de9ada936e056316e982
dosza <oliveira.daniel109@gmail.com>
2022-05-09 17:10:56 -0300

    adiciona div .widget-container e personaliza cores do botão

--- aulas/web/src/components/Widget.tsx
+++ aulas/web/src/components/Widget.tsx
@@ -2,9 +2,11 @@ import { ChatTeardropDots } from 'phosphor-react'
 
 export function Widget() {
     return (
-        <button className='bg-violet-500 rounded-full px-3 h-12'>
-            <ChatTeardropDots className='w-6 h-6'/>
-        </button>
+        <div className='widget-container'>
+            <button className='bg-brand-500 rounded-full px-3 h-12 text-white'>
+                <ChatTeardropDots className='w-6 h-6' />
+            </button>
+        </div>
     )
 
-}
+} 
