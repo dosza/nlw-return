c459e528db7aec7df586ef11ba1dbbec56bb22f8
dosza <oliveira.daniel109@gmail.com>
2022-05-10 18:30:27 -0300

    Adiciona tipos de feedback e estilização

--- aulas/web/src/components/WidgetForm.tsx
+++ aulas/web/src/components/WidgetForm.tsx
@@ -1,16 +1,54 @@
 import { CloseButton } from "./CloseButton";
+import bugImageUrl from '../assets/bug.svg'
+import ideaImageUrl from '../assets/idea.svg'
+import thoughtImageUrl from '../assets/thought.svg'
 
+const FeedbackTypes = {
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: bugImageUrl,
+            alt: 'Imagem de um inseto'
+        }
+    },
+    IDEA: {
+        title: 'Ideia',
+        image: {
+            source: ideaImageUrl,
+            alt: 'Imagem de uma lâmpada'
+        }
+    },
+    OTHER: {
+        title: 'Outro',
+        image: {
+            source: thoughtImageUrl,
+            alt: 'Imagem de um balão de pensamento'
+        }
+    }
+}
 export function WidgetForm() {
     return (
         <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4  flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>
             <header>
                 <span className="text-xl leading-6">Deixe seu Feedback</span>
-                
+
                 <CloseButton />
 
             </header>
 
-            <p>Hello World</p>
+            <div className='flex pt-8 py-8 gap-2 w-full'>
+                {Object.entries(FeedbackTypes).map(([key, value]) => {
+                    return (
+                        <button
+                            className='bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500'
+                            type='button'
+                        >
+                            <img src={value.image.source} alt={value.image.alt} />
+                            <span>{value.title}</span>
+                        </button>
+                    )
+                })}
+            </div>
 
             <footer className='text-xs text-neutral-400'>
                 Feito com ♥ pela <a className='underline underline-offset-2' href='https://www.rocketseat.com.br'>Rocketseat</a >
