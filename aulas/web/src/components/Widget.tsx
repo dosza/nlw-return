662aae0d7b522f2920827b1b23cdcad76b7afc7f
dosza <oliveira.daniel109@gmail.com>
2022-05-10 17:39:37 -0300

    Adiciona *header* e *footer* ao WidgetForm

--- aulas/web/src/components/Widget.tsx
+++ aulas/web/src/components/Widget.tsx
@@ -4,9 +4,9 @@ import { WidgetForm } from './WidgetForm'
 export function Widget() {
 
     return (
-        <Popover className='absolute bottom-5 right-5 flex flex-col items-end'>
+        <Popover className='absolute bottom-4 right-4 md:bottom-8 md:right-8   flex flex-col items-end'>
             <Popover.Panel>
-                <WidgetForm/>
+                <WidgetForm />
             </Popover.Panel>
             <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
 
