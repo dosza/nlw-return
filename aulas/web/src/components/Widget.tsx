ae7af6b26f946e0e39fda50dc0b7c920dbb718cd
dosza <oliveira.daniel109@gmail.com>
2022-05-09 23:16:06 -0300

    adiciona efeitod e transição

--- aulas/web/src/components/Widget.tsx
+++ aulas/web/src/components/Widget.tsx
@@ -5,7 +5,7 @@ export function Widget() {
         <div className='absolute bottom-5 right-5'>
             <button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
                 <ChatTeardropDots className='w-6 h-6' />
-                <span className='max-w-0 overflow-hidden group-hover:max-w-xs'>
+                <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-colors:bg-violet-700'>
                     <span className='pl-2'>Feedback</span>
                     </span>
 
