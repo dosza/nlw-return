fa674c32e0f0b3cbb05dc635fb3720a883af6871
dosza <oliveira.daniel109@gmail.com>
2022-05-10 17:59:56 -0300

    adiciona quebra de linha nas tags footer e header

--- aulas/web/src/components/WidgetForm.tsx
+++ aulas/web/src/components/WidgetForm.tsx
@@ -4,15 +4,18 @@ export function WidgetForm() {
     return (
         <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4  flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>
             <header>
-                <span className="text-xl leading-6">
-                    Deixe seu Feedback
-                </span>
-                <CloseButton/>
+                <span className="text-xl leading-6">Deixe seu Feedback</span>
+                
+                <CloseButton />
+
             </header>
+
             <p>Hello World</p>
+
             <footer className='text-xs text-neutral-400'>
-                Feito com ♥ pela <a  className='underline underline-offset-2' href='https://www.rocketseat.com.br'>Rocketseat</a >
+                Feito com ♥ pela <a className='underline underline-offset-2' href='https://www.rocketseat.com.br'>Rocketseat</a >
             </footer>
+
         </div>
     )
 }
