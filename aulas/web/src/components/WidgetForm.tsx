584ca2aafc92358a2ad09081229967c04d92c9c8
dosza <oliveira.daniel109@gmail.com>
2022-05-10 17:57:30 -0300

    Adiciona *CloseButton*

--- aulas/web/src/components/WidgetForm.tsx
+++ aulas/web/src/components/WidgetForm.tsx
@@ -1,3 +1,5 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetForm() {
     return (
         <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4  flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>
@@ -5,6 +7,7 @@ export function WidgetForm() {
                 <span className="text-xl leading-6">
                     Deixe seu Feedback
                 </span>
+                <CloseButton/>
             </header>
             <p>Hello World</p>
             <footer className='text-xs text-neutral-400'>
