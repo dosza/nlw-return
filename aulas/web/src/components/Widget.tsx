f9a698291b1a1156dbe210c5da7e73488c0058a6
dosza <oliveira.daniel109@gmail.com>
2022-05-09 23:33:40 -0300

    configura mensagem quando Widget Feedback é open

--- aulas/web/src/components/Widget.tsx
+++ aulas/web/src/components/Widget.tsx
@@ -1,9 +1,17 @@
 import { ChatTeardropDots } from 'phosphor-react'
+import { useState } from 'react'
 
 export function Widget() {
+    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+    function toggleWidgetVisibility(){
+        setIsWidgetOpen(!isWidgetOpen)
+    }
+
     return (
         <div className='absolute bottom-5 right-5'>
-            <button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+            { isWidgetOpen ? <p>Hello World!!!</p> : null  }
+            <button onClick={toggleWidgetVisibility} className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
 
                 <ChatTeardropDots className='w-6 h-6' />
 
