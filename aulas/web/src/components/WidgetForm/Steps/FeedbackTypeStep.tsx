b188ec5e1e46eab8beda4dcea2eb6c53506e6d88
dosza <oliveira.daniel109@gmail.com>
2022-05-11 12:11:43 -0300

    Move titulo de Feedback para FeedbackStep.tsx

--- aulas/web/src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ aulas/web/src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -1,4 +1,5 @@
-import { feedbackTypes , FeedbackType} from "..";
+import { feedbackTypes, FeedbackType } from "..";
+import { CloseButton } from "../../CloseButton";
 interface FeedbackTypeProps {
     onFeedbackTypeChanged: (type: FeedbackType) => void;
 }
@@ -6,20 +7,30 @@ interface FeedbackTypeProps {
 
 export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackTypeProps) {
     return (
-        <div className='flex pt-8 py-8 gap-2 w-full'>
-            {Object.entries(feedbackTypes).map(([key, value]) => {
-                return (
-                    <button
-                        key={key}
-                        onClick={() => { onFeedbackTypeChanged(key as FeedbackType) }}
-                        className='bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none'
-                        type='button'
-                    >
-                        <img src={value.image.source} alt={value.image.alt} />
-                        <span>{value.title}</span>
-                    </button>
-                )
-            })}
-        </div>
+        <>
+            <header>
+                <span className="text-xl leading-6">Deixe seu Feedback</span>
+
+                <CloseButton />
+
+            </header>
+
+
+            <div className='flex pt-8 py-8 gap-2 w-full'>
+                {Object.entries(feedbackTypes).map(([key, value]) => {
+                    return (
+                        <button
+                            key={key}
+                            onClick={() => { onFeedbackTypeChanged(key as FeedbackType) }}
+                            className='bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none'
+                            type='button'
+                        >
+                            <img src={value.image.source} alt={value.image.alt} />
+                            <span>{value.title}</span>
+                        </button>
+                    )
+                })}
+            </div>
+        </>
     );
 }
--- aulas/web/src/components/WidgetForm/index.tsx
+++ aulas/web/src/components/WidgetForm/index.tsx
@@ -33,13 +33,7 @@ export function WidgetForm() {
     const [FeedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
     return (
-        <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4  flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>
-            <header>
-                <span className="text-xl leading-6">Deixe seu Feedback</span>
-
-                <CloseButton />
-
-            </header>
+        <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4  flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>        
 
             {!FeedbackType ? (
             <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
