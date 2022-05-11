2d8d045f99c75bc4acc111d236068cfdcab4ddd3
dosza <oliveira.daniel109@gmail.com>
2022-05-11 17:24:59 -0300

    remove fragmente desnecessário em ScreenShotButton

--- aulas/web/src/components/WidgetForm/ScreenshotButton.tsx
+++ aulas/web/src/components/WidgetForm/ScreenshotButton.tsx
@@ -2,13 +2,11 @@ import { Camera } from "phosphor-react";
 
 export function ScreenshotButton() {
     return (
-        <>
-            <button
-                type='button'
-                className='p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors'
-            >
-                <Camera className='w-6 h-6' />
-            </button>
-        </>
+        <button
+            type='button'
+            className='p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors'
+        >
+            <Camera className='w-6 h-6' />
+        </button>
     )
 }
