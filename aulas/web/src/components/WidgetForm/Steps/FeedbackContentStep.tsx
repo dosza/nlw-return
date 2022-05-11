3988947a45e509eacd18e793f8932bf8e626dc5d
dosza <oliveira.daniel109@gmail.com>
2022-05-11 12:31:00 -0300

    Renderiza o tipo de feedback escolhido pelo usuário

--- aulas/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ aulas/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,28 @@
-export function FeedbackContentStep(){
+import { feedbackTypes, FeedbackType } from "..";
+import { CloseButton } from "../../CloseButton";
+
+interface FeedbackContentStepProps {
+    feedbackType: FeedbackType;
+}
+export function FeedbackContentStep({ feedbackType }: FeedbackContentStepProps) {
+    const feedbackTypeInfo  = feedbackTypes[feedbackType]
+
     return (
-        <div>
-              <p>Hello World!!</p>
-        </div>
+        <>
+            <header>
+                <span className="text-xl leading-6">
+                    <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt}/>
+                    {feedbackTypeInfo.title}
+                </span>
+
+                <CloseButton/>
+
+            </header>
+
+
+            <div className='flex pt-8 py-8 gap-2 w-full'>
+
+            </div>
+        </>
     );
 }
