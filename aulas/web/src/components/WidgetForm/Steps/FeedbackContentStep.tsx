c37310d6a8be275173d080f7334ce4e6fbf61986
dosza <oliveira.daniel109@gmail.com>
2022-05-11 15:10:15 -0300

    adiciona textarea para o usuário escrever seu feedback

--- aulas/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ aulas/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -31,6 +31,12 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequest }: 
                 <CloseButton />
             </header>
 
+            <form className='my-4 w-full'>
+                <textarea
+                    className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400  text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none'
+                    placeholder='Conte com detalhes o que está acontecendo...'
+                />
+            </form>
 
         </>
     );
