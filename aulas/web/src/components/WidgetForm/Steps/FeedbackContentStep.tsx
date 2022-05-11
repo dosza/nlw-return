df8a72cd6087231e13c7ed85bf381c5269698502
dosza <oliveira.daniel109@gmail.com>
2022-05-11 15:41:18 -0300

    adiciona estilização para escrollbar em textarea

--- aulas/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ aulas/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -33,7 +33,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequest }: 
 
             <form className='my-4 w-full'>
                 <textarea
-                    className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400  text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none'
+                    className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400  text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none scrollbar hover:scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
                     placeholder='Conte com detalhes o que está acontecendo...'
                 />
             </form>
