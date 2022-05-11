d233e4ebb354c0193a41c9f61a6165b81e822c85
dosza <oliveira.daniel109@gmail.com>
2022-05-11 13:48:50 -0300

    adiciona evento para resetar escolha de Feedback

--- aulas/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ aulas/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,28 +1,37 @@
+import { ArrowLeft } from "phosphor-react";
 import { feedbackTypes, FeedbackType } from "..";
 import { CloseButton } from "../../CloseButton";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
+    onFeedbackRestartRequest: () => void;
 }
-export function FeedbackContentStep({ feedbackType }: FeedbackContentStepProps) {
-    const feedbackTypeInfo  = feedbackTypes[feedbackType]
+export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequest }: FeedbackContentStepProps) {
+    const feedbackTypeInfo = feedbackTypes[feedbackType]
 
     return (
         <>
             <header>
-                <span className="text-xl leading-6">
-                    <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt}/>
+                <button
+                    type='button'
+                    className='top-5 left-5 absolute  text-zinc-400 hover:text-zinc-100'
+                    onClick={onFeedbackRestartRequest}
+                >
+                    <ArrowLeft weight='bold' className='w-4 h-4' />
+                </button>
+
+                <span className="text-xl leading-6 flex items-center gap-2">
+                    <img
+                        src={feedbackTypeInfo.image.source}
+                        alt={feedbackTypeInfo.image.alt}
+                        className='w-6 h-6' />
                     {feedbackTypeInfo.title}
                 </span>
 
-                <CloseButton/>
-
+                <CloseButton />
             </header>
 
 
-            <div className='flex pt-8 py-8 gap-2 w-full'>
-
-            </div>
         </>
     );
 }
