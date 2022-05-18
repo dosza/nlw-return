7b617f52d8905754eab16d85fd943c0f93d92df0
dosza <oliveira.daniel109@gmail.com>
2022-05-18 02:30:43 -0300

    adiciona botão chat (mobile)

--- aulas/mobile/src/components/Widget/index.tsx
+++ aulas/mobile/src/components/Widget/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
+import { ChatTeardropDots, } from 'phosphor-react-native'
 
 import { styles } from './styles';
+import { theme } from '../../theme';
 
 export function Widget() {
   return (
-    <View style={styles.container}>
+    <>
+      <TouchableOpacity style={styles.button}>
+        <ChatTeardropDots
+          size={24}
+          color={theme.colors.text_on_brand_color}
 
-    </View>
+        />
+
+      </TouchableOpacity>
+    </>
   );
 }
