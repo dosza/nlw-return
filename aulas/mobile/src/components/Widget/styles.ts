a6be6d6c7f593ac1197459ac95d20799fb5a7424
dosza <oliveira.daniel109@gmail.com>
2022-05-18 02:47:58 -0300

    Renderiza BottomSheet

--- aulas/mobile/src/components/Widget/styles.ts
+++ aulas/mobile/src/components/Widget/styles.ts
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 import { getBottomSpace } from 'react-native-iphone-x-helper'
 import { theme } from '../../theme';
-  
+
 export const styles = StyleSheet.create({
     button: {
         width: 48,
@@ -13,5 +13,13 @@ export const styles = StyleSheet.create({
         position: 'absolute',
         right: 16,
         bottom: getBottomSpace() + 16
+    },
+    modal: {
+        backgroundColor: theme.colors.surface_primary,
+        paddingBottom: getBottomSpace() + 16
+    },
+    indicator: {
+        backgroundColor: theme.colors.text_primary,
+        width: 56
     }
 });
