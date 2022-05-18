9fc0e891ec565bf95f1a8f7f91e6adc24817ead7
dosza <oliveira.daniel109@gmail.com>
2022-05-18 02:51:21 -0300

    corrige importação de Widget

--- aulas/mobile/App.tsx
+++ aulas/mobile/App.tsx
@@ -3,7 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
-import { Widget } from './src/components/Widget';
+import Widget from './src/components/Widget';
 import { theme } from './src/theme';
 
 
