3a02091609892facd66ce08d276c53165ae7cb52
dosza <oliveira.daniel109@gmail.com>
2022-05-18 20:46:11 -0300

    Renderiza Screenshot

--- aulas/mobile/src/components/ScreenshotButton/index.tsx
+++ aulas/mobile/src/components/ScreenshotButton/index.tsx
@@ -1,4 +1,4 @@
-import { Trash ,Camera} from 'phosphor-react-native';
+import { Trash, Camera } from 'phosphor-react-native';
 import React from 'react';
 import { View, TouchableOpacity, Image } from 'react-native';
 import { theme } from '../../theme';
@@ -19,13 +19,20 @@ export function ScreenshotButton({ screenshot, onTakeShot, onRemoveShot }: Props
         >
             {
                 screenshot ?
-                    <Trash
-                        size={22}
-                        color={theme.colors.text_secondary}
-                        weight='fill'
-                        style={styles.removeIcon}
+                    <View>
+                        <Image
+                            style={styles.image}
+                            source={{uri: screenshot}}
+                            />
+                        <Trash
+                            size={22}
+                            color={theme.colors.text_secondary}
+                            weight='fill'
+                            style={styles.removeIcon}
 
-                    /> :
+                        />
+                    </View>
+                    :
                     <Camera
                         size={24}
                         color={theme.colors.text_primary}
