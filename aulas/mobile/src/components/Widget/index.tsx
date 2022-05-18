1ba4e33c39158348f7365d0f4609525dd32a0008
dosza <oliveira.daniel109@gmail.com>
2022-05-18 10:44:14 -0300

    Inicializa componente Form (mobile)

--- aulas/mobile/src/components/Widget/index.tsx
+++ aulas/mobile/src/components/Widget/index.tsx
@@ -1,13 +1,17 @@
 import React, { useRef } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { ChatTeardropDots, } from 'phosphor-react-native'
+import { ChatTeardropDots } from 'phosphor-react-native'
 import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
+import { Options } from '../Options';
+import { Form } from '../Form';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
-import { Options } from '../Options';
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
+export type FeedbackType = keyof typeof feedbackTypes
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null)
@@ -34,7 +38,8 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Options />
+        <Form feedbackType='BUG'
+        />
 
       </BottomSheet>
     </>
