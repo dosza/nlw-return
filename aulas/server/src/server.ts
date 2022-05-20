5828c4c0f86cb3537a188f0f0b05304f02fc722d
dosza <oliveira.daniel109@gmail.com>
2022-05-20 07:17:52 -0300

    remove pacote dotenv

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -1,9 +1,6 @@
 import express from 'express'
 import { routes } from './routes'
 import cors from 'cors'
-
-import dotenv from 'dotenv'
-dotenv.config()
 const app = express()
 
 app.use(express.json())
