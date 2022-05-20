72c5fc1c3c088c7ffbebb5b7f2c388ff4965ade3
dosza <oliveira.daniel109@gmail.com>
2022-05-20 18:02:50 -0300

    Corrige estilização de CloseButton

--- web/src/components/CloseButton.tsx
+++ web/src/components/CloseButton.tsx
@@ -3,7 +3,7 @@ import { X } from 'phosphor-react'
 export function CloseButton() {
     return (
         <Popover.Button
-            className='top-5 right-5 absolute text-zinc-400 hover:text-zinc-100  rounded-sm border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none'
+            className='top-5 right-5 absolute text-zinc-400 hover:text-zinc-100  rounded-sm border-2 border-transparent focus:border-brand-500 focus:outline-none'
             title='Fechar formulário de Feedback'>
 
             <X weight='bold' className='w-4 h-4' />
