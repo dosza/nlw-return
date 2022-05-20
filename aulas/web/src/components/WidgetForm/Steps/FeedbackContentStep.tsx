3696cacaae2cbe4264330b76235aea405208eedc
dosza <oliveira.daniel109@gmail.com>
2022-05-20 03:19:24 -0300

    Desabilita botão "Enviar Feedback" quando isSendingFeedback = true

--- aulas/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ aulas/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -75,7 +75,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequest, on
 
                     <button
                         type='submit'
-                        disabled={commentFeedback.length === 0}
+                        disabled={commentFeedback.length === 0 || isSendingFeedback}
                         className='p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500'
                     >
                         {isSendingFeedback ? <Loading/> : 'Enviar Feedback' }
